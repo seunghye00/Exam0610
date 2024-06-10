22e4d744ac2514346e4587de82a808e725f7e3f6
seunghye00 <tmdgp1934@gmail.com>
2024-06-10 11:46:36 +0900

    게시판 공통 변수 패키징
    한 페이지에 노출할 게시글과 내비 수를 공통 변수로 선언 후 패키징 함.

---
+++ src/main/java/commons/BoardConfig.java
@@ -0,0 +1,10 @@
+package commons;
+
+public class BoardConfig {
+	
+	public static int RECORD_COUNT_PER_PAGE = 10;
+	// 한 페이지에 노출할 게시글 수
+	
+	public static int NAVI_COUNT_PER_PAGE = 10;
+	// 한 페이지에 노출할 내비 수
+}
